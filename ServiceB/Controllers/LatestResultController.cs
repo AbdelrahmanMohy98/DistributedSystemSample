c22e1044d469baf18a5545db84501f4cb40dbab4
Abdelrahman Mohy <abdelrahmanmhey42@gmail.com>
2026-06-13 21:34:35 +0300

    Read last result from text file

---
+++ ServiceB/Controllers/LatestResultController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceB.Services;
+
+namespace ServiceB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatestResultController : ControllerBase
+    {
+        private readonly ITextFileService _textFileService;
+
+        public LatestResultController(ITextFileService textFileService)
+        {
+            _textFileService = textFileService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<int>> LatestNumber() 
+        {
+            return Ok(await _textFileService.ReadAsync());
+        }
+    }
+}
